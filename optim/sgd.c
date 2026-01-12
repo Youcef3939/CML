d0893e824322b2d29da2b4e967daac9ac4f1c20a
Youcef <youcefchalbi39@gmail.com>
2026-01-12 18:03:15 +0100

    Refactor sgd_step function for cleaner code

--- optim/sgd.c
+++ optim/sgd.c
@@ -4,18 +4,15 @@
 
 void sgd_step(Tensor* param, float lr) {
     if (!param || !param->grad) return;
-
     for (int i = 0; i < param->size; i++) {
         param->data[i] -= lr * param->grad[i];
     }
 }
-
 void sgd_step_params(Tensor** params, int n_params, float lr) {
     for (int i = 0; i < n_params; i++) {
         sgd_step(params[i], lr);
     }
 }
-
 void sgd_zero_grad(Tensor** params, int n_params) {
     for (int i = 0; i < n_params; i++) {
         if (!params[i] || !params[i]->grad) continue;
